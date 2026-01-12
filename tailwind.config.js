60b82e56d9fce4f899ba591bb8fa0410e7cf488f
prangarajan <33352253+prangarajan@users.noreply.github.com>
2026-01-12 16:41:20 -0500

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,11 +37,11 @@ export default {
         accent: 'var(--color-accent)',
       },
       boxShadow: {
-        sm: '0 1px 2px 0 rgb(var(--color-shadow) / 0.05)',
-        DEFAULT: '0 1px 3px 0 rgb(var(--color-shadow) / 0.1), 0 1px 2px -1px rgb(var(--color-shadow) / 0.1)',
-        md: '0 4px 6px -1px rgb(var(--color-shadow) / 0.1), 0 2px 4px -2px rgb(var(--color-shadow) / 0.1)',
-        lg: '0 10px 15px -3px rgb(var(--color-shadow) / 0.1), 0 4px 6px -4px rgb(var(--color-shadow) / 0.1)',
-        xl: '0 20px 25px -5px rgb(var(--color-shadow) / 0.1), 0 8px 10px -6px rgb(var(--color-shadow) / 0.1)',
+        sm: '0 1px 2px 0 var(--color-shadow)',
+        default: '0 1px 3px 0 var(--color-shadow), 0 1px 2px -1px var(--color-shadow)',
+        md: '0 4px 6px -1px var(--color-shadow), 0 2px 4px -2px var(--color-shadow)',
+        lg: '0 10px 15px -3px var(--color-shadow), 0 4px 6px -4px var(--color-shadow)',
+        xl: '0 20px 25px -5px var(--color-shadow, 0 8px 10px -6px var(--color-shadow)',
       },
       keyframes: {
         marquee: {
