c3d6d886384b80f2b73874b1f41d7c6ff4c0e8ff
prangarajan <33352253+prangarajan@users.noreply.github.com>
2026-04-27 11:34:47 -0400

    phase b

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,26 +14,12 @@ export default {
       height: {
         'screen-dvh': '100dvh',
       },
-      textColor: {
-        body: 'var(--color-text)',
-        secondary: 'var(--color-text-secondary)',
-        accent: 'var(--color-accent)',
-        subtle: 'var(--color-text-subtle)',
-        button: 'var(--color-btn-text)',
-        control: 'var(--color-ui-text)',
-        primary: 'var(--color-text)',
-      },
-      borderColor: {
-        std: 'var(--color-border)',
-        body: 'var(--color-text)',
-        accent: 'var(--color-accent)',
-      },
       boxShadow: {
-        sm: '0 1px 2px 0 var(--color-shadow)',
-        default: '0 1px 3px 0 var(--color-shadow), 0 1px 2px -1px var(--color-shadow)',
-        md: '0 4px 6px -1px var(--color-shadow), 0 2px 4px -2px var(--color-shadow)',
-        lg: '0 10px 15px -3px var(--color-shadow), 0 4px 6px -4px var(--color-shadow)',
-        xl: '0 20px 25px -5px var(--color-shadow, 0 8px 10px -6px var(--color-shadow)',
+        sm: '0 1px 2px 0 var(--shadow)',
+        default: '0 1px 3px 0 var(--shadow), 0 1px 2px -1px var(--shadow)',
+        md: '0 4px 6px -1px var(--shadow), 0 2px 4px -2px var(--shadow)',
+        lg: '0 10px 15px -3px var(--shadow), 0 4px 6px -4px var(--shadow)',
+        xl: '0 20px 25px -5px var(--shadow), 0 8px 10px -6px var(--shadow)',
       },
       keyframes: {
         marquee: {
